14042fb4ba04910f5a0f775f99fabf60f6e304b4
artdevue <artdevue@yahoo.com>
2013-03-31 10:26:38 +0300

    add v. 0.0.4 bera

--- core/components/supercache/supercache.onwebpagecomplete.php
+++ core/components/supercache/supercache.onwebpagecomplete.php
@@ -1,46 +1,50 @@
 <?php
 $file_cache = false;
 $handler = false;
-// получаем с конфига параметры
+$sc_expires = isset($sc_expires) ? $sc_expires : 0;
+
+/* get with the config settings */
 $cache_prefix = $modx->getOption('cache_prefix');
 $cache_handler = $modx->getOption('cache_handler');
-// формируем ключ кэша
+/* form the cache key */
 $file = md5($_SERVER['REQUEST_URI']);
-// очишаем на url
-if(isset($_REQUEST[$modx->getOption('request_param_alias')])) {
-    $q = str_replace('.','_',$_REQUEST[$modx->getOption('request_param_alias')]);
-    $arrayUrl = array_filter(explode('/',$q),function($el){ return !empty($el);});
-    $pathUri = implode('/',$arrayUrl);
+/* clear the url of debris */
+if (isset($_REQUEST[$modx->getOption('request_param_alias')])) {
+    $q = str_replace('.', '_', $_REQUEST[$modx->getOption('request_param_alias')]);
+    $arrayUrl = array_filter(explode('/', $q), function ($el) {
+        return !empty($el);
+    });
+    $pathUri = implode('/', $arrayUrl);
 } else {
-	$pathUri = 'index';
-} 
-$cache_path = 'static/'.$pathUri;
-// проверяем какой кэш и существует ли файл в кэше
-switch ($cache_handler) {
-	case 'cache.xPDOAPCCache':
-		if(apc_exists($cache_path.'/'.$cache_prefix.$file)) {
-			$file_cache = true;			
-		}
-		$handler = true;
-	break;
-	case 'xPDOFileCache':
-		if(file_exists(MODX_CORE_PATH.'cache/'.$cache_path.'/'.$file.'.cache.php')) {
-			$file_cache = true;			
-		}
-		$handler = true;
-	break;
+    $pathUri = 'index';
 }
-// если файла нету в кэше и кэш у нас активный, то пишем вывод в кэш
-if($file_cache == false && $handler == true && $modx->getCacheManager()) {
-	if(isset($sc_out) && $sc_out == 1) {
-		$output = &$modx->resource->_output;
-	} else {
-		$output = &$modx->resource->_content;
-	}
-	$cacheArray = array('output'=>$output, 'time_start'=>time(), 'expires' => $sc_expires);
-	$modx->cacheManager->set($file,$cacheArray,0,array(
-	xPDO::OPT_CACHE_KEY => $cache_path,
-	xPDO::OPT_CACHE_HANDLER => $modx->getOption('cache_resource_handler', null, $modx->getOption(xPDO::OPT_CACHE_HANDLER)),
-	xPDO::OPT_CACHE_FORMAT => (integer) $modx->getOption('cache_resource_format', null, $modx->getOption(xPDO::OPT_CACHE_FORMAT, null, xPDOCacheManager::CACHE_PHP)),
-	));
+$cache_path = 'static/' . $pathUri;
+/* check the cache, and whether the cache file */
+switch ($cache_handler) {
+    case 'cache.xPDOAPCCache':
+        if (apc_exists($cache_path . '/' . $cache_prefix . $file)) {
+            $file_cache = true;
+        }
+        $handler = true;
+        break;
+    case 'xPDOFileCache':
+        if (file_exists(MODX_CORE_PATH . 'cache/' . $cache_path . '/' . $file . '.cache.php')) {
+            $file_cache = true;
+        }
+        $handler = true;
+        break;
+}
+/* The cache file is not present, and cache activity, then we write the output to a cache */
+if ($file_cache == false && $handler == true && $modx->getCacheManager()) {
+    if (isset($sc_out) && $sc_out == 1) {
+        $output = & $modx->resource->_output;
+    } else {
+        $output = & $modx->resource->_content;
+    }
+    $cacheArray = array('output' => $output, 'time_start' => time(), 'expires' => $sc_expires);
+    $modx->cacheManager->set($file, $cacheArray, 0, array(
+        xPDO::OPT_CACHE_KEY => $cache_path,
+        xPDO::OPT_CACHE_HANDLER => $modx->getOption('cache_resource_handler', null, $modx->getOption(xPDO::OPT_CACHE_HANDLER)),
+        xPDO::OPT_CACHE_FORMAT => (integer)$modx->getOption('cache_resource_format', null, $modx->getOption(xPDO::OPT_CACHE_FORMAT, null, xPDOCacheManager::CACHE_PHP)),
+    ));
 }
--- core/components/supercache/supercache_read_cache.php
+++ core/components/supercache/supercache_read_cache.php
@@ -1,84 +1,89 @@
-<?php 
-  // функция для вывода памяти (можно не использовать)
-  function convert($size) {
-   $unit=array('b','kb','mb','gb','tb','pb');
-   return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
-  }
-  // функция извлечения массива с файла
-  function geFile($key) {
-    $value= null;
+<?php
+/* function to display memory (can not use) */
+function convert($size)
+{
+    $unit = array('b', 'kb', 'mb', 'gb', 'tb', 'pb');
+    return @round($size / pow(1024, ($i = floor(log($size, 1024)))), 2) . ' ' . $unit[$i];
+}
+
+/* function retrieve an array from a file */
+function geFile($key)
+{
+    $value = null;
     if (file_exists($key)) {
-      if ($files = @fopen($key, 'rb')) {      
-        if (flock($files, LOCK_SH)) {
-          $value= @include $key;
-          flock($files, LOCK_UN);
-            if ($value === null) {
-                fclose($files);
-                @ unlink($key);
+        if ($files = @fopen($key, 'rb')) {
+            if (flock($files, LOCK_SH)) {
+                $value = @include $key;
+                flock($files, LOCK_UN);
+                if ($value === null) {
+                    fclose($files);
+                    @ unlink($key);
+                }
             }
+            @fclose($files);
         }
-        @fclose($files);
-      }
     }
     return $value;
-  } 
-  $start_memory_usage = memory_get_usage();
-  // Получаем название ключа для кэша  
-  $file = md5($_SERVER['REQUEST_URI']);
-  // достаём с кэша конфиг файл 
-  $system_cache_pach = MODX_CORE_PATH.'cache/system_settings/config.cache.php';
-  $valueFile = geFile($system_cache_pach);  
-  // формируем путь для нашего кэша
-  if(isset($_REQUEST[$valueFile['request_param_alias']])) {
-    $q = str_replace('.','_',$_REQUEST[$valueFile['request_param_alias']]);
-    $arrayUrl = array_filter(explode('/',$q),function($el){ return !empty($el);});
-    $pathUri = implode('/',$arrayUrl);
-    if(strlen($pathUri) > 0) $pathUri .= '/';
-  } else {
+}
+
+$start_memory_usage = memory_get_usage();
+/* Create a name for the cache key  */
+$file = md5($_SERVER['REQUEST_URI']);
+/* We get out of the cache configuration file */
+$system_cache_pach = MODX_CORE_PATH . 'cache/system_settings/config.cache.php';
+$valueFile = geFile($system_cache_pach);
+/* form the way for our cache */
+if (isset($_REQUEST[$valueFile['request_param_alias']])) {
+    $q = str_replace('.', '_', $_REQUEST[$valueFile['request_param_alias']]);
+    $arrayUrl = array_filter(explode('/', $q), function ($el) {
+        return !empty($el);
+    });
+    $pathUri = implode('/', $arrayUrl);
+    if (strlen($pathUri) > 0) $pathUri .= '/';
+} else {
     $pathUri = 'index/';
-  }    
-  // берём с настроек модекса префикс кэша и время жизни кэша
-  $cache_prefix = $valueFile['cache_prefix'];
-  //$cache_expires = intval($valueFile['cache_expires']);
-  // проверяем. если есть файл в кэше
-  switch ($valueFile['cache_handler']) {
-    case 'cache.xPDOAPCCache':      
-      $cache_key = "static/$pathUri$cache_prefix$file";
-      // проверяем, есть ли файл в кэше
-      if(apc_exists($cache_key)) {
-        // получаем файл с кэша
-        $value = apc_fetch($cache_key);
-        // Если его время жизни ещё не прошло или cache_expires у нас нуль, то выводим
-        if (time() < ($value['time_start'] + $value['expires']) || $value['expires'] == 0) {
-            // подсчитываем время и память
-            $time = microtime(true) - $tstart ;
-            // парсим наш тег [^t^] и заменяем на время и память
-            echo str_replace('[^t^]', 'APCCache: '.round($time,6).' s. :: Memory: '.convert(memory_get_usage() - $start_memory_usage), $value['output']); // Выводим содержимое файла 
-            exit;
-        } else {
-          // удаляем файл кэша
-          apc_delete($cache_key);
+}
+/* We take in setting MODX, prefix cache and cache lifetime */
+$cache_prefix = $valueFile['cache_prefix'];
+/* check the type of cache */
+switch ($valueFile['cache_handler']) {
+    case 'cache.xPDOAPCCache':
+        $cache_key = "static/$pathUri$cache_prefix$file";
+        /* check if there is a file in the cache */
+        if (apc_exists($cache_key)) {
+            /* get a file from the cache */
+            $value = apc_fetch($cache_key);
+            /* If the lifetime of the file has not passed or cache_expires have zero, then the output file */
+            if (time() < ($value['time_start'] + $value['expires']) || $value['expires'] == 0) {
+                /* We believe the time and memory */
+                $time = microtime(true) - $tstart;
+                /* parse our tag [^ t ^] and replace it with the time and memory */
+                echo str_replace('[^t^]', 'APCCache: ' . round($time, 6) . ' s. :: Memory: ' . convert(memory_get_usage() - $start_memory_usage), $value['output']);
+                exit;
+            } else {
+                /* delete the file cache */
+                apc_delete($cache_key);
+            }
         }
-      }               
-    break;
+        break;
     case 'xPDOFileCache':
-      $cache_file = MODX_CORE_PATH."cache/static/$pathUri$file.cache.php"; 
-      // проверяем, есть ли файл в кэше      
-      if (file_exists($cache_file)) {
-        // получаем наш файл
-        $value = geFile($cache_file);           
-        // Если его время жизни ещё не прошло или cache_expires у нас нуль, то выводим
-        if (time() < ($value['time_start'] + $value['expires']) || $value['expires'] == 0) {
-            // подсчитываем время и память
-            $time = microtime(true) - $tstart ;
-            // парсим наш тег [^t^] и заменяем на время и память
-            echo str_replace('[^t^]', 'FileCache: '.round($time,6).' s. :: Memory: '.convert(memory_get_usage() - $start_memory_usage), $value['output']); // Выводим содержимое файла
-            exit;
-        } else {
-          // удаляем файл с кэша
-          unlink($cache_file);
+        $cache_file = MODX_CORE_PATH . "cache/static/$pathUri$file.cache.php";
+        /* check if there is a file in the cache */
+        if (file_exists($cache_file)) {
+            /* get a file from the cache */
+            $value = geFile($cache_file);
+            /* If the lifetime of the file has not passed or cache_expires have zero, then the output file */
+            if (time() < ($value['time_start'] + $value['expires']) || $value['expires'] == 0) {
+                /* We believe the time and memory */
+                $time = microtime(true) - $tstart;
+                /* parse our tag [^ t ^] and replace it with the time and memory */
+                echo str_replace('[^t^]', 'FileCache: ' . round($time, 6) . ' s. :: Memory: ' . convert(memory_get_usage() - $start_memory_usage), $value['output']);
+                exit;
+            } else {
+                /* delete the file cache */
+                unlink($cache_file);
+            }
         }
-      }
-    break;
-  }
+        break;
+}
 
